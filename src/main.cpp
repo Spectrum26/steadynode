f74eaf0beec11a2fe9e7adf4b18ebf28e90cfc98
Jack <nlo22-123321@yahoo.com>
2019-01-15 18:24:10 +0300

    Revert "ch-3"
    This reverts commit 0c84a81dfbbc6ebef5d42cc3b9a2505f97f5bf66.

--- src/main.cpp
+++ src/main.cpp
@@ -2698,9 +2698,9 @@ bool CBlock::AcceptBlock()
     if (pindexPrev->GetBlockTime() > FORK_TIME)
         nTargetTmp = TARGET_SPACING2;
 
-    LogPrintf("************  nBits =  %s\n", nBits);
+    LogPrintf("************  nBits =  %s\n", nBits.ToString().c_str());
     unsigned int GetNextTargetRequiredVar = GetNextTargetRequired(pindexPrev, IsProofOfStake(), false);
-    LogPrintf("************  GetNextTargetRequired(pindexPrev, IsProofOfStake(), false) =  %s\n", GetNextTargetRequiredVar);
+    LogPrintf("************  GetNextTargetRequired(pindexPrev, IsProofOfStake(), false) =  %s\n", GetNextTargetRequiredVar.ToString().c_str());
 
     if (nBits != GetNextTargetRequired(pindexPrev, IsProofOfStake(), false) && hash != uint256("0x474619e0a58ec88c8e2516f8232064881750e87acac3a416d65b99bd61246968") && (GetBlockTime() - pindexPrev->GetBlockTime()) < NO_MINING_TIME * nTargetTmp)
         return DoS(100, error("AcceptBlock() : incorrect %s", IsProofOfWork() ? "proof-of-work" : "proof-of-stake"));
